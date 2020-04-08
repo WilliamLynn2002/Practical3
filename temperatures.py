fc4e5c723205b600bbf49e7a1733f1b4598c175d
WilliamLynn2002 <lynnmyataung@my.jcu.edu.au>
2020-04-08 21:19:54 +0630

    Get the password changed.

---
+++ temperatures.py
@@ -0,0 +1,19 @@
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print("Result: {:.2f} F".format(fahrenheit))
+    elif choice == "F":
+        # TODO: Write this section to convert F to C and display the result
+       ]
+        pass
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
